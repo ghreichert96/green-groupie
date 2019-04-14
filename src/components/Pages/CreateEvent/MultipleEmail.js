b24572557f541e358f64dfbc2179b09dfd001602
tcbvilla <43624433+tcbvilla@users.noreply.github.com>
2019-04-14 15:24:24 -0500

    Update MultipleEmail.js
    update mulipleemail

--- src/components/Pages/CreateEvent/MultipleEmail.js
+++ src/components/Pages/CreateEvent/MultipleEmail.js
@@ -26,43 +26,22 @@ class MultipleEmail extends React.Component {
             ],
 
         };
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleAddition = this.handleAddition.bind(this);
-        this.handleDrag = this.handleDrag.bind(this);
+
     }
 
-    handleDelete(i) {
-        const { tags } = this.state;
-        this.setState({
-            tags: tags.filter((tag, index) => index !== i),
-        });
-    }
 
-    handleAddition(tag) {
-        this.setState(state => ({ tags: [...state.tags, tag] }));
-    }
-
-    handleDrag(tag, currPos, newPos) {
-        const tags = [...this.state.tags];
-        const newTags = tags.slice();
-
-        newTags.splice(currPos, 1);
-        newTags.splice(newPos, 0, tag);
-
-        // re-render
-        this.setState({ tags: newTags });
-    }
 
     render() {
         const { tags} = this.state;
         return (
             <div>
-                <ReactTags tags={tags}
+                <ReactTags tags={this.props.tags}
                            placeholder = {placeholder}
-                           handleDelete={this.handleDelete}
-                           handleAddition={this.handleAddition}
-                           handleDrag={this.handleDrag}
+                           handleDelete={this.props.handleDelete}
+                           handleAddition={this.props.handleAddition}
+                           handleDrag={this.props.handleDrag}
                            delimiters={delimiters}
+                           onChange={() => {console.log('testestst')}}
                            />
             </div>
         )
