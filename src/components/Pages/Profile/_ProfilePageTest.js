8378f513fb2e32675e2d1851c6db54b41809b3d5
Yibo Pan <yibopan12@gmail.com>
2019-04-14 15:02:21 -0500

    Fix importing calendar

--- src/components/Pages/Profile/_ProfilePageTest.js
+++ src/components/Pages/Profile/_ProfilePageTest.js
@@ -28,6 +28,12 @@ class ListDividers extends Component {
     //this.listUpcomingEvents = this.listUpcomingEvents.bind(this);
     // this.handleClientLoad = this.handleClientLoad.bind(this);
   }
+  
+  componentDidMount(){
+    this.initGapi()
+    // this.handleClientLoad()
+  }
+
 
   // handleClientLoad(){
   //   gapi.client.load('calendar');
@@ -79,9 +85,52 @@ class ListDividers extends Component {
   //     })
   //   })
   // }
+  
+  
+  
+  
+  initGapi = () => {
+    console.log('Initializing GAPI...');
+    console.log('Creating the google script tag...');
 
+    const script = document.createElement("script");
+    script.onload = () => {
+      console.log('Loaded script, now loading our api...')
+      // Gapi isn't available immediately so we have to wait until it is to use gapi.
+      this.loadClientWhenGapiReady(script);
+    };
+    script.src = "https://apis.google.com/js/client.js";
+    
+    document.body.appendChild(script);
+  }
+  
+  loadClientWhenGapiReady = (script) => {
+    console.log('Trying To Load Client!');
+    console.log(script)
+    if(script.getAttribute('gapi_processed')){
+      console.log('Client is ready! Now you can access gapi. :)');
+      if(window.location.hostname==='localhost'){
+        gapi.client.load("http://localhost:8080/_ah/api/discovery/v1/apis/metafields/v1/rest")
+        .then((response) => {
+          console.log("Connected to metafields API locally.");
+          },
+          function (err) {
+            console.log("Error connecting to metafields API locally.");
+          }
+        );
+      }
+    }
+    else{
+      console.log('Client wasn\'t ready, trying again in 100ms');
+      setTimeout(() => {this.loadClientWhenGapiReady(script)}, 100);
+    }
+  }
+
+  
   render(){
+  
     const { classes } = this.props;
+    
     
     return (
       <List component="nav" className={classes.root}>
