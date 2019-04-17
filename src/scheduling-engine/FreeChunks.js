9794d65d1045642fa3ff2003656759828ba09239
deokfilho <deokcelmofilho@gmail.com>
2019-04-16 21:43:30 -0500

    testing for getTotalChunks

--- src/scheduling-engine/FreeChunks.js
+++ src/scheduling-engine/FreeChunks.js
@@ -6,14 +6,14 @@ function getUserDefs() {
         daily_str: '08:00:00',
         daily_end: '20:00:00',
         str: '2019-04-15T08:00:00-05:00',
-        end: '2019-04-15T20:00:00-05:00'
+        end: '2019-04-16T20:00:00-05:00'
     }
 }
 
 function getEventsList() {
-    return [{end: {dateTime: "2019-04-15T11:00:00-05:00"}, start: {dateTime: "2019-04-15T10:00:00-05:00"}}]
-            // {end: {dateTime: "2019-04-02T21:00:00-05:00"}, start: {dateTime: "2019-04-02T19:30:00-05:00"}},
-            // {end: {dateTime: "2019-04-24T08:40:00-05:00"}, start: {dateTime: "2019-04-23T22:10:00-05:00"}},
+    return [{end: {dateTime: "2019-04-15T11:00:00-05:00"}, start: {dateTime: "2019-04-15T10:00:00-05:00"}},
+            {end: {dateTime: "2019-04-16T20:30:00-05:00"}, start: {dateTime: "2019-04-16T21:00:00-05:00"}},
+            {end: {dateTime: "2019-04-24T08:40:00-05:00"}, start: {dateTime: "2019-04-23T22:10:00-05:00"}}]
             // {end: {dateTime: "2019-04-24T15:33:00-05:00"}, start: {dateTime: "2019-04-24T14:00:00-05:00"}},
             // {end: {dateTime: "2019-04-01T15:55:00-05:00"}, start: {dateTime: "2019-04-01T14:10:00-05:00"}},
             // {end: {dateTime: "2019-04-02T07:00:00-05:00"}, start: {dateTime: "2019-04-01T20:20:00-05:00"}},
@@ -22,7 +22,7 @@ function getEventsList() {
             // {end: {dateTime: "2019-04-18T07:55:00-05:00"}, start: {dateTime: "2019-04-18T06:15:00-05:00"}},
             // {end: {dateTime: "2019-04-26T18:45:00-05:00"}, start: {dateTime: "2019-04-26T17:06:00-05:00"}}]
 }
-
+// TODO: deal with wrong events(start after end)
 
 function getTotalChunks() {
     let totalChunks = [];
@@ -32,14 +32,22 @@ function getTotalChunks() {
     const last_event = events[events.length - 1];
     if (events) {
         if (curr_ev.start.dateTime > window.str) {
-            totalChunks.push({str: curr_ev.start.dateTime, end: window.str});
+            totalChunks.push({str: window.str, end: curr_ev.start.dateTime});
         }
-        if (window.end > last_event.end) {
-            totalChunks.push({str: last_event.end, end: window.end});
+        if (window.end > last_event.end.dateTime) {
+            totalChunks.push({str: last_event.end.dateTime, end: window.end});
         }
         if (events.length >=2) { events.forEach(function (event) {
-            totalChunks.push({str:curr_ev.end.dateTime, end: event.start.dateTime});
-            curr_ev = event;
+            if (curr_ev.end.dateTime < event.start.dateTime) {
+                if (window.end > event.start.dateTime) {
+                    totalChunks.push({str:curr_ev.end.dateTime, end: event.start.dateTime});
+                }
+                else {
+
+                }
+
+                curr_ev = event;
+            }
         })}
       }
     return totalChunks
--- src/scheduling-engine/__tests__/FreeChunks.tests.ts
+++ src/scheduling-engine/__tests__/FreeChunks.tests.ts
@@ -11,16 +11,22 @@ test('test_getUserDefs', () => {
     );
 });
 
-test('test_getTotalChunks', () => {
-    expect(getEventsList()[0].start.dateTime).toEqual(
-        "2019-04-15T10:00:00-05:00"
-    );
-});
+// test('test_getTotalChunks', () => {
+//     expect(getEventsList()[0].start.dateTime).toEqual(
+//         "2019-04-15T10:00:00-05:00"
+//     );
+// });
+
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T08:00:00-05:00",end:"2019-04-15T10:00:00-05:00"},
+//                     {str:"2019-04-15T11:00:00-05:00", end:"2019-04-15T20:00:00-05:00"}]
+//     );
+// });
 
 test('test_getTotalChunks', () => {
     expect(getTotalChunks()).toEqual(
-        [{end:"2019-04-15T10:00:00-05:00", str:"2019-04-15T08:00:00-05:00"},
-                    {end:"2019-04-15T20:00:00-05:00", str:"2019-04-15T11:00:00-05:00"}]
+        [{str:"2019-04-15T08:00:00-05:00",end:"2019-04-15T10:00:00-05:00"},
+            {str:"2019-04-15T11:00:00-05:00", end:"2019-04-15T20:00:00-05:00"}]
     );
 });
-
