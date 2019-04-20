7755ba22468c1c6ac77ccd0441e9c774f4bd8dd2
deokfilho <deokcelmofilho@gmail.com>
2019-04-20 15:52:04 -0500

    all preliminary tests passed for getTotalChunks

--- src/scheduling-engine/FreeChunks.js
+++ src/scheduling-engine/FreeChunks.js
@@ -4,18 +4,22 @@ function getUserDefs() {
     // make firebase request to get user definitions
     return {
         daily_str: '09:00:00',
-        daily_end: '09:00:00',
+        daily_end: '21:00:00',
         str: '2019-04-15T09:00:00-05:00',
-        end: '2019-04-17T10:00:00-05:00',
+        end: '2019-04-15T21:00:00-05:00',
         duration: 30
 
     }
 }
 
 function getEventsList() {
-    return [{end: {dateTime: "2019-04-15T11:00:00-05:00"}, start: {dateTime: "2019-04-15T10:00:00-05:00"}},
-            {end: {dateTime: "2019-04-16T20:30:00-05:00"}, start: {dateTime: "2019-04-16T21:00:00-05:00"}},
-            {end: {dateTime: "2019-04-24T08:40:00-05:00"}, start: {dateTime: "2019-04-23T22:10:00-05:00"}}]
+    return [{end: {dateTime: "2019-04-15T09:00:00-05:00"},
+            start: {dateTime: "2019-04-15T08:00:00-05:00"}},
+            {end: {dateTime: "2019-04-17T21:33:00-05:00"},
+            start: {dateTime: "2019-04-17T21:00:00-05:00"}}]
+            // {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+            // start: {dateTime: "2019-04-17T07:00:00-05:00"}}]
+    // {end: {dateTime: "2019-04-24T08:40:00-05:00"}, start: {dateTime: "2019-04-T22:10:00-05:00"}}]
             // {end: {dateTime: "2019-04-24T15:33:00-05:00"}, start: {dateTime: "2019-04-24T14:00:00-05:00"}},
             // {end: {dateTime: "2019-04-01T15:55:00-05:00"}, start: {dateTime: "2019-04-01T14:10:00-05:00"}},
             // {end: {dateTime: "2019-04-02T07:00:00-05:00"}, start: {dateTime: "2019-04-01T20:20:00-05:00"}},
@@ -40,16 +44,19 @@ function getTotalChunks() {
             totalChunks.push({str: last_event.end.dateTime, end: window.end});
         }
         if (events.length >=2) { events.forEach(function (event) {
-            if (curr_ev.end.dateTime < event.start.dateTime) {
-                if (window.end > event.start.dateTime) {
-                    totalChunks.push({str:curr_ev.end.dateTime, end: event.start.dateTime});
+            if (curr_ev !== event) {
+                if (curr_ev.end.dateTime < event.start.dateTime) {
+                    if (window.end >= event.start.dateTime) {
+                        totalChunks.push({str:curr_ev.end.dateTime, end: event.start.dateTime});
+                    }
+                    else {}
+                    curr_ev = event;
                 }
                 else {
-
+                    curr_ev = event;
                 }
-
-                curr_ev = event;
             }
+
         })}
       }
     return totalChunks
@@ -60,23 +67,32 @@ function getFreeChunks() {
     const totalChunks = getTotalChunks();
     const window = getUserDefs();
     totalChunks.forEach(function(chunk) {
-        if (chunk.str.day === chunk.end.day) {
-            // TODO: create helper fucnction to create and return chunk objects
-            if ((chunk.end <= window.daily_str) || (chunk.str >= window.daily_end)) {
+        let day_str = chunk.str.split('T')[0];
+        let day_end = chunk.end.split('T')[0];
+        let timezone = chunk.str.split('-')[-1];
+        let daily_str = day_str + "T" + window.daily_str + "-" + timezone;
+        let daily_end = day_end + "T" + window.daily_end + "-" + timezone;
+        if (day_str === day_end) {
+            // TODO: create helper function to create and return chunk objects
+            if ((chunk.end <= daily_str) || (chunk.str >= window.daily_end)) {
                 // ignore
             }
             else {
                 if((chunk.str >= window.daily_str) && (chunk.end <= window.daily_end)) {
                     //    new chunk, starting at chunk str, ending at chunk end
+                    freeChunks.push({str: chunk.str, end: chunk.end})
                 }
                 else if ((chunk.str < window.daily_str) && (chunk.end <= window.daily_end)) {
                     //    new chunk, starting at daily str, ending at chunk end
+                    freeChunks.push({str: daily_str, end: chunk.end})
                 }
                 else if ((chunk.end > window.daily_end) && (chunk.str >= window.daily_str)) {
                     //    new chunk, starting at chunk str, ending at daily end
+                    freeChunks.push({str: chunk.str , end: daily_end})
                 }
                 else {
                     //    new chunk, starting at daily str, ending at daily end
+                    freeChunks.push({str: daily_str , end: daily_end})
                 }
             }
 
@@ -88,14 +104,14 @@ function getFreeChunks() {
                 if (day === chunk.str.day) {
                     if (chunk.str < window.daily_str) {
                         //    new chunk, starting at daily str, ending at daily end
-                        createInsertChunk(freeChunks,window.daily_str, window.daily_end)
+                        freeChunks.push({str: window.daily_str, end: window.daily_end})
                     }
                     else if (chunk.str > window.daily_end) {
                         //ignore
                     }
                     else {
                         //    new chunk, starting at chunk str, ending at daily end
-                        createInsertChunk(freeChunks,chunk.str, window.daily_end)
+                        freeChunks.push({str: chunk.str, end: daily_end})
                     }
                 }
                 else if (day === chunk.end.day) {
@@ -104,16 +120,16 @@ function getFreeChunks() {
                     }
                     else if (chunk.end > window.daily_end) {
                         //    new chunk, starting at daily str, ending at daily end
-                        createInsertChunk(freeChunks,window.daily_str, window.daily_end)
+                        freeChunks.push({str: daily_str, daily_end})
                     }
                     else {
                         //    new chunk, starting at daily str, ending at chunk end
-                        createInsertChunk(freeChunks,window.daily_str, chunk.end)
+                        freeChunks.push({str: daily_str, end: chunk.end})
                     }
                 }
                 else {
                     //    new chunk, starting at daily str, ending at daily end
-                    createInsertChunk(freeChunks,window.daily_str, window.daily_end)
+                    freeChunks.push({str: daily_str, end: daily_end})
                 }
             }
         }
@@ -121,8 +137,5 @@ function getFreeChunks() {
     return freeChunks
 }
 
-function createInsertChunk(freeChunkList, start, end) {
-    freeChunkList.push({str: start, end: end})
-}
 
-export { getTotalChunks } ;
+export { getTotalChunks, getFreeChunks } ;
--- src/scheduling-engine/__tests__/FreeChunks.test.js
+++ src/scheduling-engine/__tests__/FreeChunks.test.js
@@ -1,4 +1,4 @@
-import { getTotalChunks } from '../FreeChunks';
+import { getTotalChunks, getFreeChunks } from '../FreeChunks';
 
 // test('test_getUserDefs', () => {
 //     expect(getUserDefs()).toEqual(
@@ -10,6 +10,10 @@ import { getTotalChunks } from '../FreeChunks';
 //         }
 //     );
 // });
+
+
+// --------------------- START OF TOTAL CHUNKS TESTS ---------------------------
+
 
 // test('test_getTotalChunks', () => {
 //     expect(getEventsList()[0].start.dateTime).toEqual(
@@ -24,9 +28,110 @@ import { getTotalChunks } from '../FreeChunks';
 //     );
 // });
 
-test('test_getTotalChunks', () => {
-    expect(getTotalChunks()).toEqual(
-        [{str:"2019-04-15T08:00:00-05:00",end:"2019-04-15T10:00:00-05:00"},
-            {str:"2019-04-15T11:00:00-05:00", end:"2019-04-15T20:00:00-05:00"}]
+// Slots end at window start and start at window end
+// PASS
+// events = [{end: {dateTime: "2019-04-15T09:00:00-05:00"},
+//     start: {dateTime: "2019-04-15T08:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T10:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-17T10:00:00-05:00"}]
+//     );
+// });
+
+// Slots end past wdw start, and start before window end, huge chunk in mdiddle
+// PASS
+// events = [{end: {dateTime: "2019-04-15T09:30:00-05:00"},
+//     start: {dateTime: "2019-04-15T08:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T09:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:30:00-05:00", end:"2019-04-17T09:00:00-05:00"}]
+//     );
+// });
+
+// Slots past window start, before window end, and event in middle of the day
+// PASS
+// event = [{end: {dateTime: "2019-04-15T09:30:00-05:00"},
+//     start: {dateTime: "2019-04-15T08:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-16T15:33:00-05:00"},
+//         start: {dateTime: "2019-04-16T14:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T09:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:30:00-05:00", end:"2019-04-16T14:00:00-05:00"},
+//                     {str:"2019-04-16T15:33:00-05:00", end:"2019-04-17T09:00:00-05:00"}]
+//     );
+// });
+
+//many slots throughout the day PASS
+// events = [{end: {dateTime: "2019-04-15T12:30:00-05:00"},
+//     start: {dateTime: "2019-04-15T10:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-15T15:33:00-05:00"},
+//         start: {dateTime: "2019-04-15T14:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T09:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-15T10:00:00-05:00"},
+//             {str:"2019-04-15T12:30:00-05:00", end:"2019-04-15T14:00:00-05:00"},
+//             {str:"2019-04-15T15:33:00-05:00", end:"2019-04-17T09:00:00-05:00"}]
+//     );
+// });
+
+// overlapping events PASS
+// events = [{end: {dateTime: "2019-04-15T12:30:00-05:00"},
+//     start: {dateTime: "2019-04-15T10:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-15T15:33:00-05:00"},
+//         start: {dateTime: "2019-04-15T12:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T09:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-15T10:00:00-05:00"},
+//             {str:"2019-04-15T15:33:00-05:00", end:"2019-04-17T09:00:00-05:00"}]
+//     );
+// });
+
+// complicated overlap(2 overlaps, ending past window end) PASS
+// events = [{end: {dateTime: "2019-04-15T12:30:00-05:00"},
+//     start: {dateTime: "2019-04-15T10:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T08:33:00-05:00"},
+//         start: {dateTime: "2019-04-15T12:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-17T10:30:00-05:00"},
+//         start: {dateTime: "2019-04-17T07:00:00-05:00"}}]
+// test('test_getTotalChunks', () => {
+//     expect(getTotalChunks()).toEqual(
+//         [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-15T10:00:00-05:00"}]
+//     );
+// });
+
+// --------------------- END OF TOTAL CHUNKS TESTS ---------------------------
+
+
+// --------------------- START OF FREE CHUNKS TESTS ---------------------------
+
+// free chunk, entire day free, only one day PASS
+// events = [{end: {dateTime: "2019-04-15T09:00:00-05:00"},
+//     start: {dateTime: "2019-04-15T08:00:00-05:00"}},
+//     {end: {dateTime: "2019-04-15T21:33:00-05:00"},
+//         start: {dateTime: "2019-04-15T21:00:00-05:00"}}]
+// test('test_getFreeChunks', () => {
+//     expect(getFreeChunks()).toEqual(
+//         [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-15T21:00:00-05:00"}]
+//     );
+// });
+
+// Two entire days free, should break into two full day chunks
+test('test_getFreeChunks', () => {
+    expect(getFreeChunks()).toEqual(
+        [{str:"2019-04-15T09:00:00-05:00", end:"2019-04-15T21:00:00-05:00"},
+                    {str:"2019-04-17T09:00:00-05:00", end:"2019-04-17T21:00:00-05:00"}]
     );
 });
+
+// --------------------- END OF FREE CHUNKS TESTS ---------------------------
+
